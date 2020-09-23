dd9b5f08e57e5f03736a83a8e5e9e5f6dfef864c
Tom <tom@Maxs-MacBook-Pro.local>
2020-09-23 10:55:06 +1000

    Cleaned entire error handling up

--- src/script.js
+++ src/script.js
@@ -15,34 +15,41 @@ const callEventsApi = async (month, day) => { // gets events
             clearError();
             displayFacts(day, month, randomEvent.year, randomEvent.description, "Event")
         }) // returns our result
+        .catch((err) => {
+           if (err instanceof SyntaxError){
+               displayError('Invalid input')
+           } else if (err instanceof TypeError){
+               displayError('Unable to contact API')
+           } else {
+               displayError('Please try again')
+           }
+        })
+}
+// TypeError: Failed to fetch---- console logged out all different error types, googled how to handle these
+//if (e instanceof SyntaxError) {
+// } else if (e instanceof TypeError) {
+//     // statements to handle RangeError exceptions
+const callDeathApi = async (month, day) => { // gets deaths
+    await fetch(`https://byabbe.se/on-this-day/${month}/${day}/deaths.json`)
+        .then(async (res) => {
+            const result = await res.json() // this is gonna fetch API returns 'readableStream', you must run .json() to convert it to JSON. same as up^^
+            const deathList = result.deaths;
+            const randomDeath = deathList[Math.floor(Math.random() * deathList.length)];
+            clearError();
+            displayFacts(day, month, randomDeath.year, randomDeath.description, "Death")
+        }) // return our result
         .catch(() => displayError()) // displays error
 }
     
-const callDeathApi = async (month, day) => { // gets deaths
-    const call = await fetch(`https://byabbe.se/on-this-day/${month}/${day}/deaths.json`)
-    .then((res) => res) // return our result
-    .catch((err) => err) // returns the error
-    const result = await call.json() // this is gonna fetch API returns 'readableStream', you must run .json() to convert it to JSON. same as up^^
-
-    const deathList = result.deaths;
-    const randomDeath = deathList[Math.floor(Math.random() * deathList.length)];
-
-
-    displayFacts(day, month, randomDeath.year, randomDeath.description, "Death")
-}
-    
-const callBirthApi = async (month,day) => { // gets births
-    const call = await fetch(`https://byabbe.se/on-this-day/${month}/${day}/births.json`)
-    .then((res) => res) // return result
-    .catch((err) => err) // return error
-
-    const result = await call.json() // Fetch API returns 'readableStream', you must run .json() to convert it to JSON.
-    const eventList = result.births;
-
-    const randomBirth = eventList[Math.floor(Math.random() * eventList.length)];
-
-    displayFacts(day, month, randomBirth.year, randomBirth.description, "Birth")
-
+const callBirthApi = async (month, day) => { // gets births
+    await fetch(`https://byabbe.se/on-this-day/${month}/${day}/births.json`)
+        .then(async (res) => {
+            const result = await res.json() // Fetch API returns 'readableStream', you must run .json() to convert it to JSON.
+            const eventList = result.births;
+            const randomBirth = eventList[Math.floor(Math.random() * eventList.length)];
+            displayFacts(day, month, randomBirth.year, randomBirth.description, "Birth")
+        }) // return result
+        .catch(() => displayError()) // return error
 }
 
 const callAllApi = (month, day) => { // gets one of each type
@@ -75,7 +82,6 @@ const randomFact = () => { // feelin historical
     let randomNumber = Math.floor(Math.random() * 4) // 0-3 to choose from array ^
     
     return (functionArray[randomNumber])(randomMonth, randomDay)
-    
 }
 
 const clearBtn = () => { // clears everything
@@ -91,39 +97,35 @@ const displayFacts = (day, month, year, fact, type) => { // function that takes 
     let liHeading = document.createElement("li") // create heading
     let li = document.createElement("li") 
 
-    
-    
     let factText = document.createTextNode(`${day}/${month}/${year} - ${fact}`)
 
     // li.appendChild(dateText)
-    
-
     liHeading.textContent = `${type}:` // display heading as type given
     liHeading.classList = "fact-heading"
     li.prepend(factText)
+
     // li.textContent = `${day}/${month}/${year} - ${fact}`
     li.classList = "fact-text"
 
     let ul = document.getElementById("fact-ul")
     ul.prepend(li) // append the fact and date to the ul
     ul.prepend(liHeading)
-    if (type == "birth"){
+    if (type == "Birth"){
         li.style.backgroundColor = "#94ff94"
-    } else if (type == "death"){
+    } else if (type == "Death"){
         li.style.backgroundColor = "#666666"
         li.style.color = "white"
-    } else if (type == "event"){
+    } else if (type == "Event"){
         li.style.backgroundColor = "#a3dcff"
     }
-    
     if (ul.hasChildNodes() == true){
         button.style.display = "block"
     } 
     
 }
 
-const displayError = () => {
-    document.querySelector('#error-div').innerHTML = 'Error - please try again';
+const displayError = (error) => {
+    document.querySelector('#error-div').innerHTML = `Error: ${error}`;
 }
 
 const clearError = () => {
