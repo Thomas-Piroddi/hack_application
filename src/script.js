5c7b77da682be3c3fa6fe55b8d1522c963a46610
MeikaFreckelton <meikafreckelton@gmail.com>
2020-09-23 10:58:37 +1000

    trying to pull

--- src/script.js
+++ src/script.js
@@ -1,20 +1,24 @@
+let button = document.getElementById("clear-btn") // allow button to be accessible outside of functions
+button.style.display = "none"
+let ul = document.getElementById("fact-ul")
 
-const callEventsApi = async (month, day) => {
-    const call = await fetch(`https://byabbe.se/on-this-day/${month}/${day}/events.json`)
-    .then((res) => res) // returns our result
-    .catch((err) => err) // returns the error
-    
-    const result = await call.json() // this is gonna fetch API returns 'readableStream', you must run .json() to convert it to JSON. weird thing just to change it back to json
-    const eventList = result.events;
-    const randomEvent = eventList[Math.floor(Math.random() * eventList.length)];
+if (ul.hasChildNodes() == false){
+    button.style.display = "none"
+} 
 
-
-
-    displayFacts(day, month, randomEvent.year, randomEvent.description, "event")
-    
+const callEventsApi = async (month, day) => { // gets events
+    await fetch(`https://byabbe.se/on-this-day/${month}/${day}/events.json`)
+        .then(async (res) => {
+            const result = await res.json() // this is gonna fetch API returns 'readableStream', you must run .json() to convert it to JSON. weird thing just to change it back to json
+            const eventList = result.events;
+            const randomEvent = eventList[Math.floor(Math.random() * eventList.length)];
+            clearError();
+            displayFacts(day, month, randomEvent.year, randomEvent.description, "Event")
+        }) // returns our result
+        .catch(() => displayError()) // displays error
 }
     
-const callDeathApi = async (month, day) => {
+const callDeathApi = async (month, day) => { // gets deaths
     const call = await fetch(`https://byabbe.se/on-this-day/${month}/${day}/deaths.json`)
     .then((res) => res) // return our result
     .catch((err) => err) // returns the error
@@ -24,10 +28,10 @@ const callDeathApi = async (month, day) => {
     const randomDeath = deathList[Math.floor(Math.random() * deathList.length)];
 
 
-    displayFacts(day, month, randomDeath.year, randomDeath.description, "death")
+    displayFacts(day, month, randomDeath.year, randomDeath.description, "Death")
 }
     
-const callBirthApi = async (month,day) => {
+const callBirthApi = async (month,day) => { // gets births
     const call = await fetch(`https://byabbe.se/on-this-day/${month}/${day}/births.json`)
     .then((res) => res) // return result
     .catch((err) => err) // return error
@@ -37,11 +41,11 @@ const callBirthApi = async (month,day) => {
 
     const randomBirth = eventList[Math.floor(Math.random() * eventList.length)];
 
-    displayFacts(day, month, randomBirth.year, randomBirth.description, "birth")
+    displayFacts(day, month, randomBirth.year, randomBirth.description, "Birth")
 
 }
 
-const callAllApi = (month, day) => { // display one of each type
+const callAllApi = (month, day) => { // gets one of each type
 
     let event = callEventsApi(month, day)
     let birth = callBirthApi(month, day)
@@ -49,14 +53,48 @@ const callAllApi = (month, day) => { // display one of each type
 
 }
 
-const displayFacts = (day, month, year, fact, type) => { // function that takes in the year and the fact and displays them
+const randomFact = () => { // feelin historical
+    // get random number between one and 3 for events, births and deaths
+    // get random date - (random 1-12 for months), (random 1-30) 
+    // consider different month lengths 
+    // 30 days - sep, april, june, nov / 4, 6, 9, 11
+    // 31 days - jan, march, may, july, august, oct, dec
+    // keep it max 29 for feb
+
+    let randomMonth = Math.floor(Math.random() * 12) + 1 // get a random month
+    let randomDay = 0
+    if (randomMonth == 2){ // if february - 1-29
+        randomDay = Math.floor(Math.random() * 29) + 1 // months with 30 days - 1-30
+    } else if (randomMonth == 4 || randomMonth == 6 || randomMonth == 9 || randomMonth == 11){
+        randomDay = Math.floor(Math.random() * 30) + 1
+    } else { // the rest - 1-31
+        randomDay = Math.floor(Math.random() * 31) + 1
+    }
+
+    let functionArray = [callEventsApi, callBirthApi, callDeathApi] 
+    let randomNumber = Math.floor(Math.random() * 4) // 0-3 to choose from array ^
+    
+    return (functionArray[randomNumber])(randomMonth, randomDay)
+    
+}
+
+const clearBtn = () => { // clears everything
+    let ul = document.getElementById("fact-ul")
+    ul.remove() // clear all facts
+    location.reload() // reload page - clear form
+
+}
+
+// -- // -- //
+
+const displayFacts = (day, month, year, fact, type) => { // function that takes in the date, fact and type and displays them
     let liHeading = document.createElement("li") // create heading
     let li = document.createElement("li") 
 
-    // not sure which is best practice so leaving them both in here for now :
-
-    // let dateText = document.createTextNode(``) 
+    
+    
     let factText = document.createTextNode(`${day}/${month}/${year} - ${fact}`)
+
     // li.appendChild(dateText)
     
 
@@ -67,8 +105,8 @@ const displayFacts = (day, month, year, fact, type) => { // function that takes 
     li.classList = "fact-text"
 
     let ul = document.getElementById("fact-ul")
-    // ul.prepend(liHeading)
-    ul.prepend(liHeading,li) // append the fact and date to the ul
+    ul.prepend(li) // append the fact and date to the ul
+    ul.prepend(liHeading)
     if (type == "birth"){
         li.style.backgroundColor = "#94ff94"
     } else if (type == "death"){
@@ -78,29 +116,16 @@ const displayFacts = (day, month, year, fact, type) => { // function that takes 
         li.style.backgroundColor = "#a3dcff"
     }
     
+    if (ul.hasChildNodes() == true){
+        button.style.display = "block"
+    } 
+    
 }
 
-const randomFact = () => {
-    // get random number between one and 3 for events, births and deaths
-    // get random date - (random 1-12 for months), (random 1-30) 
-    // consider different month lengths 
-    // 30 days - sep, april, june, nov / 4, 6, 9, 11
-    // 31 days - jan, march, may, july, august, oct, dec
-    // keep it max 28 for feb
+const displayError = () => {
+    document.querySelector('#error-div').innerHTML = 'Error - please try again';
+}
 
-    let randomMonth = Math.floor(Math.random() * 12) + 1
-    let randomDay = 0
-    if (randomMonth == 2){
-        randomDay = Math.floor(Math.random() * 29) + 1 
-    } else if (randomMonth == 4 || randomMonth == 6 || randomMonth == 9 || randomMonth == 11){
-        randomDay = Math.floor(Math.random() * 30) + 1
-    } else {
-        randomDay = Math.floor(Math.random() * 31) + 1
-    }
-
-    let functionArray = [callEventsApi, callBirthApi, callDeathApi]
-    let randomNumber = Math.floor(Math.random() * 4)
-    
-    return (functionArray[randomNumber])(randomMonth, randomDay)
-    
+const clearError = () => {
+    document.querySelector('#error-div').innerHTML = '';
 }
